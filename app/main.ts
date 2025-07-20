11eb02c54ad9249d3a42ab030e72fe4ab33661fd
MurtazaD1410 <dhariwalamurtaza22@gmail.com>
2025-07-20 22:48:02 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -121,12 +121,30 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
     if (command?.toUpperCase() === "LPOP") {
       const listName = commandArgs[0];
       const list = listMap[listName];
+      let itemCount = 0;
+      let itemsToDel = 1;
+
+      let outputStr = "";
       if (!list) {
         connection.write(`$-1\r\n`);
       } else {
-        const removed = list[0];
-        list.shift();
-        connection.write(`$${removed.length}\r\n${removed}\r\n`);
+        if (!commandArgs[1]) {
+          outputStr += `$${list[0].length}\r\n${list[0]}\r\n`;
+          list.shift();
+          connection.write(outputStr);
+          return;
+        }
+        if (commandArgs[1]) {
+          itemsToDel = parseInt(commandArgs[1]);
+        }
+        for (let i = 0; i < itemsToDel; i++) {
+          outputStr += `$${list[i].length}\r\n${list[i]}\r\n`;
+          list.shift();
+          i--;
+          itemCount++;
+          itemsToDel--;
+        }
+        connection.write(`*${itemCount}\r\n${outputStr}`);
       }
     }
   });
